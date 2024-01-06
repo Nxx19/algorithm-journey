20e7abeea890d91969036dd4b5679cd4c57bac24
Administrator <xiaoyi19n@126.com>
2024-01-06 12:58:21 +0800

    test01

---
+++ src/test/Demo02.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * @Author nfx
+ * @Create 2023/10/24
+ * Description:
+ */
+public class Demo01 {
+
+    public static void main(String[] args) {
+
+        System.out.println(123);
+
+    }
+
+}
